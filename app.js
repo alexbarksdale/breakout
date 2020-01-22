610f8b7032b8a3d54c197363663272d0e451184b
alexbarksdale <ale.barksdale@gmail.com>
2020-01-21 22:39:46 -0800

    Add 📝 | Collision detection

--- app.js
+++ app.js
@@ -1,6 +1,55 @@
 const canvas = document.getElementById('my-canvas');
 const ctx = canvas.getContext('2d');
 
+// ==============================
+// BRICK VARIABLES
+const BRICK_ROW_COUNT = 3;
+const BRICK_COLUMN_COUNT = 5;
+const BRICK_WIDTH = 75;
+const BRICK_HEIGHT = 20;
+const BRICK_PADDING = 10;
+const BRICK_OFF_SET_TOP = 30;
+const BRICK_OFF_SET_LEFT = 30;
+
+const bricks = [];
+// Loops through the rows and columns and creates the bricks
+for (let column = 0; column < BRICK_COLUMN_COUNT; column += 1) {
+    bricks[column] = [];
+    for (let row = 0; row < BRICK_ROW_COUNT; row += 1) {
+        bricks[column][row] = { x: 0, y: 0, status: 1 };
+    }
+}
+const drawBricks = () => {
+    for (let column = 0; column < BRICK_COLUMN_COUNT; column += 1) {
+        for (let row = 0; row < BRICK_ROW_COUNT; row += 1) {
+            if (bricks[column][row].status === 1) {
+                const BRICK_X = column * (BRICK_WIDTH + BRICK_PADDING) + BRICK_OFF_SET_LEFT;
+                const BRICK_Y = row * (BRICK_WIDTH + BRICK_PADDING) + BRICK_OFF_SET_TOP;
+                bricks[column][row].x = BRICK_X;
+                bricks[column][row].y = BRICK_Y;
+                ctx.beginPath();
+                ctx.rect(BRICK_X, BRICK_Y, BRICK_WIDTH, BRICK_HEIGHT);
+                ctx.fillStyle = '#F7705E';
+                ctx.fill();
+            }
+        }
+    }
+};
+
+// ==============================
+// BALL PROPERTIES
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+const BALL_RADIUS = 10;
+
+const drawBall = () => {
+    ctx.beginPath();
+    ctx.arc(x, y, BALL_RADIUS, 0, Math.PI * 2); // x,y,width,height
+    ctx.fillStyle = '#ffffff';
+    ctx.fill(); // automatcally closes
+};
+
+// ==============================
 // PADDLE CONTROLS
 let rightClick = false;
 let leftClick = false;
@@ -24,18 +73,6 @@ const keyUpHandler = (e) => {
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
 
-// BALL PROPERTIES
-let x = canvas.width / 2;
-let y = canvas.height - 30;
-const BALL_RADIUS = 10;
-
-const drawBall = () => {
-    ctx.beginPath();
-    ctx.arc(x, y, BALL_RADIUS, 0, Math.PI * 2); // x,y,width,height
-    ctx.fillStyle = '#0095DD';
-    ctx.fill(); // automatcally closes
-};
-
 // PADDLE PROPERTIES
 const PADDLE_HEIGHT = 10;
 const PADDLE_WIDTH = 75;
@@ -44,18 +81,42 @@ let paddleX = (canvas.width - PADDLE_WIDTH) / 2;
 const drawPaddle = () => {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT);
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = '#89BCC8';
     ctx.fill();
 };
 
+// ==============================
 // PIXELS TO MOVE THE BALL (SPEED)
 let dx = 2;
 let dy = -2;
 
+// ==============================
+// COLLISION PROPERTIES
+const collisionDetection = () => {
+    for (let column = 0; column < BRICK_COLUMN_COUNT; column += 1) {
+        for (let row = 0; row < BRICK_ROW_COUNT; row += 1) {
+            const brick = bricks[column][row];
+            if (brick.status === 1) {
+                if (
+                    x > brick.x &&
+                    x < brick.x + BRICK_WIDTH &&
+                    y > brick.y &&
+                    y < brick.y + BRICK_HEIGHT
+                ) {
+                    dy = -dy;
+                    brick.status = 0;
+                }
+            }
+        }
+    }
+};
+
 const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height); // clears canvas content
+    drawBricks();
     drawBall();
     drawPaddle();
+    collisionDetection();
 
     // Bounce off the left and right
     if (x + dx > canvas.width - BALL_RADIUS || x + dx < BALL_RADIUS) {
@@ -63,21 +124,22 @@ const draw = () => {
     }
 
     // Bounce off the top and bottom
-    if (y + dy > canvas.height - BALL_RADIUS || y + dy < BALL_RADIUS) {
+    if (y + dy < BALL_RADIUS) {
         dy = -dy;
+    } else if (y + dy > canvas.height - BALL_RADIUS) {
+        if (x > paddleX && x < paddleX + PADDLE_WIDTH) {
+            dy = -dy;
+        } else {
+            alert('GAME OVER');
+            document.location.reload();
+            clearInterval(interval);
+        }
     }
 
-    if (rightClick) {
+    if (rightClick && paddleX < canvas.width - PADDLE_WIDTH) {
         paddleX += 7;
-
-        if (paddleX + PADDLE_WIDTH > canvas.width) {
-            paddleX = canvas.width - PADDLE_WIDTH;
-        }
-    } else if (leftClick) {
+    } else if (leftClick && paddleX > 0) {
         paddleX -= 7;
-        if (paddleX < 0) {
-            paddleX = 0;
-        }
     }
 
     // Moves the ball
@@ -85,4 +147,4 @@ const draw = () => {
     y += dy;
 };
 
-setInterval(draw, 10);
+const interval = setInterval(draw, 10);
